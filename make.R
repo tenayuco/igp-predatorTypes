aa05a8c16da541244995cd14b10f61557e2cdf02
Emilio Mora <emiliomora92@gmail.com>
2026-03-25 11:11:07 +0100

    change steps in make R, and change names

--- make.R
+++ make.R
@@ -1,4 +1,7 @@
 
+
+#====================ALWAYS RUN THIS===================
+
 # Look for every package used in the project
 # Add them to DESCRIPTION under Imports
 rdeps::add_deps()
@@ -13,31 +16,47 @@ devtools::load_all()
 ### these instructions are not linear, follow them according to your needs
 
 ###
-#roxygen2::roxygenise()
 #################################################
 #1. Set the inital conditions used for the rest of the simulations (within the environment)
 source("analyses/01.set_initial_condition.R")
 
+
+
+
+
+#=============USER OPTIONAL FUNCTIONS======================
+
+##=======================BIOCONTROL PLOTS============================
+
 #################################################
-#2. Calculate a plot the herbivore density for different values of s, and K. (enter that script to modify the conditions)
+# Calculate a plot the herbivore density for different values of s, and K. (enter that script to modify the conditions)
 source("analyses/02.set_df_biocontrol.R")
 ####################3
 
-
-#3. plot thegeneral figures (of biocoontr)
+# plot thegeneral figures (of biocoontr)
 source("analyses/03.analyze_biocontrol.R")
+
+
+#============================ DETAILED NUMERIC BIFURCATIONS======================
 
 start<-proc.time()
 #################################################
-#4. derives the detailed bifurcations of examples.. enter the script to change the conditons 
+#Derives the detailed bifurcations of examples.. enter the script to change the conditons 
 source("analyses/04.set_df_examples.R")
 ####################3
 print(proc.time()- start)
 
 #################################################
-#5. plots detailed bifurcations of examples.. enter the script to change the conditons 
-#source("analyses/05.plot_detailed_bifurcation.R")
+#Plots detailed bifurcations of examples.. enter the script to change the conditons 
+source("analyses/05.plot_detailed_bifurcation.R")
 ####################3
+
+
+#================ MANUAL EXPLORATIONS===========================
+
+source("analyses/06.bif_manual_exploration.R")
+
+
 
 
 
