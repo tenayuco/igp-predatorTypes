1ef2d262426ff3aa2e7dceb8d2ea6590d8f0048c
Emilio Mora <emiliomora92@gmail.com>
2026-04-17 01:21:51 +0200

    just added some specific tests on the debif

--- analyses/06.bif_manual_exploration.R
+++ analyses/06.bif_manual_exploration.R
@@ -19,11 +19,11 @@
 ## 1. First you chose the parameters you want MANUALLY
 
 chosenP <- "LB" 
-chosenN <- "HV"
+chosenN <- "PA"
 ipProp <- 0.1  ##or 0.9
 inProp <- 0.1  ## or 0.9
-phiP <- 0.5
-phiN <-  0.5
+phiP <- 0
+phiN <-  0
 
 
 ##. 2. Then you run the function that will opne a shiny app. In this you can:
@@ -59,3 +59,49 @@ saveRDS(bifDATA , paste0(output_dir, "/",
                         "phiN_", phiN, ".RData"))
 
 ##4. you will be able to use this data to extract information from it. 
+
+
+
+##########################33
+
+
+
+
+
+LBLB_LV_log_model <- function (t, state, parms) {##includes PB, LB as predators and as preys
+  
+  with(as.list(c(state, parms)), {
+    ##the i is the same for both predators 
+  
+    ###predator IGP P functional responses
+    ### prey IGP N functional response
+    
+    #these are the equations 
+    dPdt <- Ep*((frp*S*R + fnp*N*(1-S))*P)- mup*P
+    dNdt <- En*(frn*R*N)- fnp*P*N-mun*N 
+    dRdt <- rho*R*(1-R/K)- (frn*N- frp*P)*R  #logisstic
+    
+    # z[R, Nl, Na, P] is the order of the vector
+    return(list(c(dRdt, dNdt,  dPdt)))        
+  })
+}
+
+
+LBLB_LV_parms <- c(
+  rho = 1,
+  K = 5,
+  En = 0.5,
+  Ep = 0.5,
+  frp = 0.6,   ### if wwe suppose both r and n to be biomass, we extract that when n=r=1/2 hn (to reach the linear assumtoption), then frp should be cp/4
+  fnp = 0.6,
+  frn = 5, ## hrtr is cn/2
+  mun = 1.1,
+  mup = 0.33, ## to compare with hin, they have mun = 1 (Tp) + 0.3,  and mup = 0.3 (maintenance) + 0.03 death rate
+ #here is dfi
+  S = 0.5
+) #here the Sr different
+
+
+deBif::bifurcation(LBLB_LV_log_model, c(R=0.1, N=0.1, P=0.1), LBLB_LV_parms,  resume = TRUE) #this creates myl1BifCurves
+
+
